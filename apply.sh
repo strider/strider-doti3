b074c76e3e2bb6424ddac05927abdb3dcdeb7128
Gael Chamoulaud <gchamoul@redhat.com>
2018-11-20 16:31:29 +0100

    Update 11/20/2018
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- apply.sh
+++ apply.sh
@@ -8,4 +8,4 @@ cp -rvf i3/.i3status.conf ~/.i3/
 ln -vsf ~/.i3/dunstrc ~/.config/dunst/dunstrc
 cp -vf mpdmenu.sh ~/bin/
 cp -vf passmenu ~/bin
-sudo cp -rvf ./keyboard-backlight.sh /usr/local/bin/
+#sudo cp -rvf ./keyboard-backlight.sh /usr/local/bin/
