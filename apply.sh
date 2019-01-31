ac2cbfd00d5b43228c6d1e40d6faf24c3fc0dc6b
Gael Chamoulaud <gchamoul@redhat.com>
2019-01-31 13:01:53 +0100

    Update 01/31/2019
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- apply.sh
+++ apply.sh
@@ -7,4 +7,5 @@ cp -rvf i3/*  ~/.i3/
 cp -rvf i3/.i3status.conf ~/.i3/
 ln -vsf ~/.i3/dunstrc ~/.config/dunst/dunstrc
 cp -vf passmenu ~/bin
+cp -vf i3-touchpad ~/bin
 #sudo cp -rvf ./keyboard-backlight.sh /usr/local/bin/
