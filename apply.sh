e1662b5a925f08f6260ca9c45dd2740765eae1a7
Gael Chamoulaud <gchamoul@redhat.com>
2019-04-11 11:49:27 +0200

    New Rofi theme
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- apply.sh
+++ apply.sh
@@ -6,8 +6,8 @@ mkdir -p ~/.i3
 cp -rvf i3/*  ~/.i3/
 cp -rvf i3/.i3status.conf ~/.i3/
 cp -rvf i3/.i3status-rust.toml
+cp -rvf rofi.rasi ~/.i3/
 ln -vsf ~/.i3/dunstrc ~/.config/dunst/dunstrc
 cp -vf passmenu ~/bin
 cp -vf i3-touchpad ~/bin
 cp -vf keyboard ~/bin
-#sudo cp -rvf ./keyboard-backlight.sh /usr/local/bin/
