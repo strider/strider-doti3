4f0b4ebb4b1b59d4da8c732f17bc238446e5197b
Gael Chamoulaud <gchamoul@redhat.com>
2019-04-11 11:48:45 +0200

    Use i3status-rust
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- apply.sh
+++ apply.sh
@@ -5,6 +5,7 @@ rm -Rvf ~/.i3
 mkdir -p ~/.i3
 cp -rvf i3/*  ~/.i3/
 cp -rvf i3/.i3status.conf ~/.i3/
+cp -rvf i3/.i3status-rust.toml
 ln -vsf ~/.i3/dunstrc ~/.config/dunst/dunstrc
 cp -vf passmenu ~/bin
 cp -vf i3-touchpad ~/bin
