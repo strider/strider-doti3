a487e31e2ade574363b49f36ef77092e0b9c2436
Gael Chamoulaud <gchamoul@redhat.com>
2019-07-02 17:17:17 +0200

    Update 2019-07-02
    Signed-off-by: Gael Chamoulaud <gchamoul@redhat.com>

--- mybar.sh
+++ mybar.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-~/bin/bumblebee-status/./bumblebee-status -m title \
-                                          dnf  \
-                                          nic \
-                                          publicip \
-                                          layout \
-                                          dunst  \
-                                          brightness \
+notmuch_query="tag:unread AND folder:redhat-gmail/INBOX"
+
+~/bin/bumblebee-status/./bumblebee-status -m nic \
+                                          vpn \
+                                          notmuch_count \
                                           battery \
                                           pasink \
                                           pasource \
+                                          dunst \
                                           datetime \
                                           -p nic.states="^down" \
                                           root.path=/  \
                                           nic.format="{intf} {ip} {ssid}" \
                                           battery.showdevice=true \
+                                          notmuch_count.query="${notmuch_query}" \
                                           -t powerline
