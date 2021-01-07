4e61aab6726932e578c5ceddeadb4d4405fe62f5
Gael Chamoulaud (Strider) <gchamoul@redhat.com>
2021-01-07 22:08:56 +0100

    Move scripts into a bin directory
    Signed-off-by: Gael Chamoulaud (Strider) <gchamoul@redhat.com>

--- apply.sh
+++ apply.sh
@@ -7,15 +7,13 @@ rm -Rvf ~/.config/alacritty
 
 mkdir -p ~/.i3
 cp -rvf i3/*  ~/.i3/
-cp -rvf i3/.i3status.conf ~/.i3/
-cp -rvf i3/.i3status-rust.toml ~/.i3/
 cp -rvf rofi.rasi ~/.i3/
-cp -rvf i3-bindsym.sh ~/.i3/
 ln -vsf ~/.i3/dunstrc ~/.config/dunst/dunstrc
-cp -vf passmenu ~/bin
-cp -vf i3-touchpad ~/bin
-cp -vf keyboard ~/bin
-cp -vf mybar.sh ~/bin
 
-cp -rvf conky ~/.config/
-cp -rvf alacritty ~/.config/
+for script in `ls bin/`; do
+    cp -vf bin/${script} ~/bin/
+done
+
+for tool in conky alacritty; do
+    cp -rvf $tool ~/.config/
+done
