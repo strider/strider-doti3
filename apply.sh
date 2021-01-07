77164b0efe5ad395648562540acf8c19d2d63ad9
Gael Chamoulaud (Strider) <gchamoul@redhat.com>
2021-01-07 21:26:53 +0100

    Add i3-bindsym.sh script to list all my bindings with rofi
    Signed-off-by: Gael Chamoulaud (Strider) <gchamoul@redhat.com>

--- apply.sh
+++ apply.sh
@@ -7,6 +7,7 @@ cp -rvf i3/*  ~/.i3/
 cp -rvf i3/.i3status.conf ~/.i3/
 cp -rvf i3/.i3status-rust.toml ~/.i3/
 cp -rvf rofi.rasi ~/.i3/
+cp -rvf i3-bindsym.sh ~/.i3/
 ln -vsf ~/.i3/dunstrc ~/.config/dunst/dunstrc
 cp -vf passmenu ~/bin
 cp -vf i3-touchpad ~/bin
